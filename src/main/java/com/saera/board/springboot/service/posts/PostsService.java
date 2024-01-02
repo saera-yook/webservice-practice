b2c6760fdea22b18d2ce06e5de7a2bacf55a9fef
saera <yook.saera@gmail.com>
2024-01-02 22:03:09 +0900

    feat: 게시판 기능을 구현하라

---
+++ src/main/java/com/saera/board/springboot/service/posts/PostsService.java
@@ -0,0 +1,55 @@
+package com.saera.board.springboot.service.posts;
+
+import com.saera.board.springboot.domain.posts.Posts;
+import com.saera.board.springboot.domain.posts.PostsRepository;
+import com.saera.board.springboot.web.dto.PostsListResponseDto;
+import com.saera.board.springboot.web.dto.PostsResponseDto;
+import com.saera.board.springboot.web.dto.PostsSaveRequestDto;
+import com.saera.board.springboot.web.dto.PostsUpdateRequestDto;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+}
